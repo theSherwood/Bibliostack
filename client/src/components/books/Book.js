390a86e14de2de75f698748ea32fb99f52af6923
admls <adam.the.sherwood@gmail.com>
2019-05-16 11:47:24 +0100

    adds expansion panels

--- client/src/components/books/Book.js
+++ client/src/components/books/Book.js
@@ -1,7 +1,14 @@
-import React, { useEffect, useState, useRef, useReducer } from "react";
-// import { connect } from "react-redux";
-// import { getBooks } from "../../actions/bookActions";
+import React, { useEffect, useRef, useState, useReducer } from "react";
 import TextField from "@material-ui/core/TextField";
+import ExpansionPanel from "@material-ui/core/ExpansionPanel";
+import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
+import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
+import ExpansionPanelActions from "@material-ui/core/ExpansionPanelActions";
+import Typography from "@material-ui/core/Typography";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import Button from "@material-ui/core/Button";
+import Divider from "@material-ui/core/Divider";
+import { withStyles } from "@material-ui/core/styles";
 import axios from "axios";
 
 const initialState = {
@@ -13,53 +20,121 @@ const initialState = {
 function reducer(state, action) {
   switch (action.type) {
     case "fetching":
-      return { fetching: true };
+      return { ...state, fetching: true };
     case "setEbayBookResult":
-      return { fetching: false, ebayBookResult: action.payload };
+      return { ...state, fetching: false, ebayBookResult: action.payload };
     case "clearResult":
-      return { ebayBookResult: [] };
+      return { ...state, ebayBookResult: [] };
     case "setError":
-      return { fetching: false, error: action.payload };
+      return { ...state, fetching: false, error: action.payload };
     case "clearError":
-      return { error: null };
+      return { ...state, error: null };
     default:
       return state;
   }
 }
 
+const styles = theme => ({
+  root: {
+    width: "100%"
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(15)
+  },
+  secondaryHeading: {
+    fontSize: theme.typography.pxToRem(15),
+    color: theme.palette.text.secondary
+  },
+  icon: {
+    verticalAlign: "bottom",
+    height: 20,
+    width: 20
+  },
+  details: {
+    alignItems: "center"
+  },
+  column: {
+    flexBasis: "33.33%"
+  },
+  helper: {
+    borderLeft: `2px solid ${theme.palette.divider}`,
+    padding: `${theme.spacing.unit}px ${theme.spacing.unit * 2}px`
+  },
+  link: {
+    color: theme.palette.primary.main,
+    textDecoration: "none",
+    "&:hover": {
+      textDecoration: "underline"
+    }
+  }
+});
+
 const Book = props => {
-  const { getBooks } = props;
+  const { fetchResults, classes, expand } = props;
   const [state, dispatch] = useReducer(reducer, initialState);
-  const scriptElmnt = useRef(null);
+  const [expanded, setExpanded] = useState(false);
 
   useEffect(() => {
-    const body = {
-      title: "the lord of the rings",
-      author: "Tolkien",
-      price: ""
-    };
-    axios
-      .post("/api/books/book", body)
-      .then(res => {
-        const books = res.data;
-        console.log(books);
-        dispatch({ type: "setEbayBookResult", payload: books });
-      })
-      .catch(err => dispatch({ type: "setError", payload: err }));
-  }, []);
+    setExpanded(expand);
+  }, [expand]);
+
+  useEffect(() => {
+    if (fetchResults) {
+      dispatch({ type: "fetching" });
+      const body = {
+        title: "the lord of the rings",
+        author: "Tolkien",
+        price: ""
+      };
+      axios
+        .post("/api/books/book", body)
+        .then(res => {
+          const books = res.data;
+          console.log(books);
+          dispatch({ type: "setEbayBookResult", payload: books });
+        })
+        .catch(err => dispatch({ type: "setError", payload: err }));
+    }
+  }, [fetchResults]);
 
   return (
-    <div>
-      <TextField onClick={() => console.log("hello")} /> <TextField />{" "}
-      <TextField />
-      {state.error}
-      <ul>
-        {state.ebayBookResult.map(book => (
-          <li>{book.title[0]}</li>
-        ))}
-      </ul>
+    <div className={classes.root}>
+      <ExpansionPanel expanded={expanded}>
+        <ExpansionPanelSummary
+          expandIcon={<ExpandMoreIcon onClick={() => setExpanded(!expanded)} />}
+        >
+          <TextField onClick={() => console.log("hello")} />
+          {"  "} <TextField />
+          {"   "}
+          <TextField />
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails className={classes.details}>
+          {state.fetching ? <p>Fetching results...</p> : null}
+          {state.error ? state.error.message : null}
+          <ul>
+            {state.ebayBookResult.map(book => {
+              const currentPrice = book.sellingStatus[0].currentPrice[0];
+              const currency = currentPrice["@currencyId"];
+              const value = currentPrice["__value__"];
+              return (
+                <li key={book.itemId[0]}>
+                  {currency} {value + " : "}
+                  <Typography noWrap={true}>{book.title[0]}</Typography>
+                </li>
+              );
+            })}
+          </ul>
+        </ExpansionPanelDetails>
+        {/* <Divider />
+        <ExpansionPanelActions>
+          <Button size="small">Cancel</Button>
+          <Button size="small" color="primary">
+            Save
+          </Button>
+        </ExpansionPanelActions> */}
+      </ExpansionPanel>
     </div>
   );
 };
 
-export default Book;
+export default withStyles(styles)(Book);
