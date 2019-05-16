b4a5f9ccde1f559e37da4799d60c6bbfd4edf984
admls <adam.the.sherwood@gmail.com>
2019-05-16 10:40:38 +0100

    adds material ui components

--- client/src/App.js
+++ client/src/App.js
@@ -8,12 +8,18 @@ import { setUser } from "./actions/authActions";
 
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
+import SignIn from "./components/auth/SignIn";
 import HandleJWT from "./components/auth/HandleJWT";
 import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
 import Home from "./components/home/Home";
 // import PrivateRoute from "./components/routing/PrivateRoute";
 import NotFound from "./components/routing/NotFound";
+import Booklist from "./components/books/Booklist";
+
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { MuiThemeProvider } from "@material-ui/core/styles";
+import theme from "./theme";
 
 import "./App.css";
 
@@ -28,28 +34,33 @@ if (token) {
 export default function App() {
   return (
     <Provider store={store}>
-      <Router>
-        <div
-          className="App"
-          style={{
-            minHeight: "100vh",
-            position: "relative",
-            paddingBottom: "100px"
-          }}
-        >
-          <Header />
-          <div>
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/register" component={Register} />
-              <Route exact path="/login" component={Login} />
-              <Route exact path="/jwt/:token" component={HandleJWT} />
-              <Route path="/" component={NotFound} />
-            </Switch>
+      <CssBaseline />
+      <MuiThemeProvider theme={theme}>
+        <Router>
+          <div
+            className="App"
+            style={{
+              minHeight: "100vh",
+              position: "relative",
+              paddingBottom: "100px"
+            }}
+          >
+            <Header />
+            <div>
+              <Switch>
+                {/* <Route exact path="/" component={Home} /> */}
+                <Route exact path="/" component={SignIn} />
+                <Route exact path="/list" component={Booklist} />
+                <Route exact path="/register" component={Register} />
+                <Route exact path="/login" component={Login} />
+                <Route exact path="/jwt/:token" component={HandleJWT} />
+                <Route path="/" component={NotFound} />
+              </Switch>
+            </div>
+            <Footer />
           </div>
-          <Footer />
-        </div>
-      </Router>
+        </Router>
+      </MuiThemeProvider>
     </Provider>
   );
 }
