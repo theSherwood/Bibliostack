2410a1f2e74654fc194f0b46f0a4fb896e49007c
admls <adam.the.sherwood@gmail.com>
2019-05-16 10:43:25 +0100

    adds reducer for book actions

---
+++ client/src/reducers/bookReducer.js
@@ -0,0 +1,12 @@
+import {} from "../actions/types";
+
+const initialState = {
+  books: []
+};
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import authReducer from "./authReducer";
+import bookReducer from "./bookReducer";
 
 export default combineReducers({
-  auth: authReducer
+  auth: authReducer,
+  book: bookReducer
 });
