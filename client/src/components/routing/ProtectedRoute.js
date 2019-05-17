37c4df9a76aac9d24710f583fae46d458668f56d
admls <adam.the.sherwood@gmail.com>
2019-05-17 17:00:16 +0100

    changes privateRoute to ProtectedRoute

---
+++ client/src/components/routing/ProtectedRoute.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+
+const ProtectedRoute = ({
+  component: Component,
+  isAuthenticated,
+  accessType,
+  ...rest
+}) =>
+  accessType === "guest" ? (
+    // Guest only routes redirect to booklist
+    <Route
+      {...rest}
+      render={props =>
+        isAuthenticated ? (
+          <Redirect to="/booklist" />
+        ) : (
+          <Component {...props} {...rest} />
+        )
+      }
+    />
+  ) : (
+    // Private routes redirect to sign in
+    <Route
+      {...rest}
+      render={props =>
+        !isAuthenticated ? (
+          <Redirect to="/sign/in" />
+        ) : (
+          <Component {...props} {...rest} />
+        )
+      }
+    />
+  );
+
+export default ProtectedRoute;
