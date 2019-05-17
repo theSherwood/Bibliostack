f6d0c738a84559ab3d86e505375a2b5348a8e156
admls <adam.the.sherwood@gmail.com>
2019-05-17 15:06:35 +0100

    delete redux stuff and auth routes

--- client/src/actions/bookActions.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-import {} from "./types";
-
-// Get books
-export const getBooks = books => dispatch => {
-  axios.get("/api/books", books).then(res => console.log(res.data));
-  // .then(books => console.log(books));
-};
--- client/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import authReducer from "./authReducer";
-import bookReducer from "./bookReducer";
-
-export default combineReducers({
-  auth: authReducer,
-  book: bookReducer
-});
