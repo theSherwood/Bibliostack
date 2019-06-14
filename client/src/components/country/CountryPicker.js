af88bf28bd61a868616cd0016f730502b9858e82
admls <adam.the.sherwood@gmail.com>
2019-06-14 18:54:13 +0100

    adds icons

--- client/src/components/country/CountryPicker.js
+++ client/src/components/country/CountryPicker.js
@@ -1,20 +1,26 @@
 import React from "react";
+import IconAU from "./IconAU";
+import IconCA from "./IconCA";
+import IconUK from "./IconUK";
+import IconUS from "./IconUS";
 
-const countryCode = {
-  "EBAY-US": "",
-  "EBAY-ENCA": "",
-  "EBAY-GB": "",
-  "EBAY-AU": ""
+const countries = {
+  "EBAY-US": IconUS,
+  "EBAY-ENCA": IconCA,
+  "EBAY-GB": IconUK,
+  "EBAY-AU": IconAU
 };
 
-const CountryPicker = () => {
+const getIcon = countryCode => countries[countryCode] || countries["EBAY-US"];
+
+const CountryPicker = ({ countryCode, updateCountryCode }) => {
   return (
     <div>
-      <select>
-        {Object.keys(countryCode).map(code => (
-          <option>{code}</option>
-        ))}
-      </select>
+      {Object.entries(countries).map(([code, Svg]) => (
+        <Svg size="24" />
+      ))}
+
+      {/* <div>Icons made by <a href="https://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" 			    title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" 			    title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div> */}
     </div>
   );
 };
