fcea12b77fb7d97e116978181eb0d5971a5f258f
admls <adam.the.sherwood@gmail.com>
2019-06-14 19:05:22 +0100

    show buttons on click

--- client/src/components/country/CountryPicker.js
+++ client/src/components/country/CountryPicker.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import IconAU from "./IconAU";
 import IconCA from "./IconCA";
 import IconUK from "./IconUK";
@@ -14,11 +14,29 @@ const countries = {
 const getIcon = countryCode => countries[countryCode] || countries["EBAY-US"];
 
 const CountryPicker = ({ countryCode, updateCountryCode }) => {
+  const [showIcons, setShowIcons] = useState(false);
+
+  const CountryIcon = getIcon(countryCode);
+
+  const handleClick = e => {
+    setShowIcons(!showIcons);
+  };
+
   return (
     <div>
-      {Object.entries(countries).map(([code, Svg]) => (
-        <Svg size="24" />
-      ))}
+      <button onClick={handleClick}>
+        <CountryIcon size="24" />
+      </button>
+
+      {showIcons
+        ? Object.entries(countries)
+            .filter(([code, Icon]) => code !== countryCode)
+            .map(([code, Icon]) => (
+              <button onClick={handleClick}>
+                <Icon size="24" />
+              </button>
+            ))
+        : null}
 
       {/* <div>Icons made by <a href="https://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" 			    title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" 			    title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div> */}
     </div>
