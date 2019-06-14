dd05cd233a39b6754a538ed3518948b561d5452d
admls <adam.the.sherwood@gmail.com>
2019-06-14 13:36:08 +0100

    redirect to booklist at / if authenticated

--- client/src/components/auth/Auth.js
+++ client/src/components/auth/Auth.js
@@ -5,8 +5,6 @@ import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import FormControl from "@material-ui/core/FormControl";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
 import Input from "@material-ui/core/Input";
 import InputLabel from "@material-ui/core/InputLabel";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
@@ -21,7 +19,7 @@ function Auth(props) {
   const { classes, dispatch, history, isAuthenticated } = props;
   console.log("auth", isAuthenticated);
   if (isAuthenticated) {
-    history.push("/booklist");
+    history.push("/");
   }
   const [errors, setErrors] = useState({
     email: null,
