96d38b49d0f90d5808ca69f01028674eb75c1f71
admls <adam.the.sherwood@gmail.com>
2019-06-17 23:10:00 +0100

    white text and component overrides

--- client/src/theme.js
+++ client/src/theme.js
@@ -4,15 +4,43 @@ import deepPurple from "@material-ui/core/colors/deepPurple";
 const theme = createMuiTheme({
   palette: {
     primary: {
-      main: "#e65100"
+      main: "#ffffff", //"#e65100"
+      contrastText: "#ffffff",
+      text: "#ffffff",
+      light: "#ffffff",
+      dark: "#ffffff"
     },
-    secondary: deepPurple
+    secondary: {
+      main: "#ffffff", // "#b2668a"
+      contrastText: "#ffffff",
+      text: "#ffffff",
+      light: "#ffffff",
+      dark: "#ffffff"
+    },
+    text: {
+      primary: "#ffffff",
+      secondary: "#ffffff",
+      disabled: "#ffffff",
+      hint: "#ffffff"
+    }
   },
   shape: {
     borderRadius: 0
   },
   typography: {
     useNextVariants: true
+  },
+  overrides: {
+    MuiSvgIcon: {
+      root: {
+        fill: "white"
+      }
+    },
+    MuiExpansionPanelSummary: {
+      focused: {
+        background: "transparent"
+      }
+    }
   }
 });
 
