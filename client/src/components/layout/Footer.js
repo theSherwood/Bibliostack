7d9f70e9bdab7d0b0908691cf70f40ea76094e62
admls <adam.the.sherwood@gmail.com>
2019-06-17 23:12:22 +0100

    match styles of the rest of the page

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -1,17 +1,43 @@
 import React from "react";
+import { withStyles, Typography, Link } from "@material-ui/core";
 
-export default function Footer() {
+const styles = theme => ({
+  footer: {
+    position: "absolute",
+    bottom: "0px",
+    left: "0px",
+    right: "0px",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    flexWrap: "wrap"
+  },
+  typo: {
+    paddingLeft: theme.spacing.unit,
+    paddingRight: theme.spacing.unit
+  }
+});
+
+function Footer({ classes }) {
   return (
-    <footer
-      style={{
-        position: "absolute",
-        background: "steelblue",
-        bottom: "0px",
-        left: "0px",
-        right: "0px"
-      }}
-    >
-      Copyright &copy; 2019
+    <footer className={classes.footer}>
+      <Typography className={classes.typo}>
+        Copyright &copy; 2019 Adam Sherwood
+      </Typography>
+      <Typography className={classes.typo}>
+        <span>
+          Icons made by{" "}
+          <Link href="https://www.freepik.com/" title="Freepik">
+            Freepik
+          </Link>{" "}
+          from{" "}
+          <Link href="https://www.flaticon.com/" title="Flaticon">
+            www.flaticon.com
+          </Link>
+        </span>
+      </Typography>
     </footer>
   );
 }
+
+export default withStyles(styles)(Footer);
