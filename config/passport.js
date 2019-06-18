847c26aa36c09cf65a7fab1969d2f924879ec0c6
admls <adam.the.sherwood@gmail.com>
2019-06-18 20:59:33 +0100

    remove oauth

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,3 @@
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const GitHubStrategy = require("passport-github2").Strategy;
 const JwtStrategy = require("passport-jwt").Strategy;
 const ExtractJwt = require("passport-jwt").ExtractJwt;
 const mongoose = require("mongoose");
@@ -23,67 +21,6 @@ module.exports = passport => {
         })
         .catch(err => console.log(err));
     })
-  );
-
-  passport.use(
-    new GoogleStrategy(
-      {
-        clientID: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "/auth/google/callback",
-        proxy: true
-      },
-      (accessToken, refreshToken, profile, done) => {
-        const newUser = {
-          googleID: profile.id,
-          handle: profile.name.givenName || profile.name.familyName,
-          email: profile.emails[0].value,
-          image: profile.photos[0].value
-        };
-        // Check for existing user
-        User.findOne({
-          googleID: profile.id
-        }).then(user => {
-          if (user) {
-            // Return user
-            done(null, user);
-          } else {
-            // Create user
-            new User(newUser).save().then(user => done(null, user));
-          }
-        });
-      }
-    )
-  );
-
-  passport.use(
-    new GitHubStrategy(
-      {
-        clientID: process.env.GITHUB_CLIENT_ID,
-        clientSecret: process.env.GITHUB_CLIENT_SECRET,
-        callbackURL: "/auth/github/callback",
-        proxy: true
-      },
-      (accessToken, refreshToken, profile, done) => {
-        const newUser = {
-          githubId: profile.id,
-          handle: profile.displayName || profile.username,
-          email: profile._json.email,
-          image: profile.photos[0].value
-        };
-        User.findOne({
-          githubId: profile.id
-        }).then(user => {
-          if (user) {
-            // Return user
-            done(null, user);
-          } else {
-            // Create user
-            new User(newUser).save().then(user => done(null, user));
-          }
-        });
-      }
-    )
   );
 
   passport.serializeUser((user, done) => {
